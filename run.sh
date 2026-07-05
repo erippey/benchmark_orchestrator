3082d7b95ee1dab584db71ac498c8610ad986c32
erippey3 <erippey3@vt.edu>
2026-07-05 18:30:31 -0400

    new bfs numbers

--- run.sh
+++ run.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-python3 ./main.py BFS_PI_PoCL_config.json 
-python3 ./main.py FFT_PI_PoCL_config.json
-python3 ./main.py KMeans_Pi_PoCL_config.json
-python3 ./main.py SPMV_PI_PoCL_config.json
-python3 ./main.py SRAD_PI_PoCL_config.json
+python3 ./main.py BFS_PI_CPU_config.json -o
+
 
--- run2.sh
+++ run2.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-
-python3 ./main_nano.py SPMV_nano_clvk_config.json -o
-python3 ./main_nano.py SRAD_nano_clvk_config.json -o
 python3 ./main_nano.py BFS_nano_clvk_config.json
 python3 ./main_nano.py FFT_nano_clvk_config.json
 python3 ./main_nano.py KMeans_nano_clvk_config.json
